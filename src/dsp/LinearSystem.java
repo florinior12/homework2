5b31fa691b90efdeab82130900645a95f0f9d445
Florin Iordache <florin.gal12@gmail.com>
2017-01-27 11:00:51 +0200

    comments

--- src/dsp/LinearSystem.java
+++ src/dsp/LinearSystem.java
@@ -1,5 +1,4 @@
 package dsp;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,23 +11,20 @@ public class LinearSystem {
     private ArrayList<Element> sortedElements = new ArrayList<>();
     private Signal input = new Signal();
     private Signal output;
-
-    public LinearSystem() {
-
+    //PLEASE CHANGE THE ACCESS OF samples[] field of the Signal class, delay field of Delay, samples[] field of Filter and gain field of Gain to implicit access modifier
+    //The LinearSystem(String fileName) constructor works by calling the setInput method and then the getOutput method, so there needs to be just one Signal declaration in main, the input signal
+    public LinearSystem() { //Empty system
     }
 
 
     public LinearSystem(String fileName) throws IOException {
         BufferedReader bf = null;
-
-
-        bf = new BufferedReader(new FileReader(fileName));
         HashMap hashMap = new HashMap();
-
+        bf = new BufferedReader(new FileReader(fileName));
         String line = bf.readLine();
         String text = "";   //Whole text file is kept in this String
 
-        while ((line != null)) {
+        while ((line != null)) {    //While loop used to read all the lines of the text file
             if (!line.equals(""))
                 text += line + "\n";
             line = bf.readLine();
@@ -39,17 +35,23 @@ public class LinearSystem {
         int k = 0;  //For debug purposes
 
         for (String instruction : instructions) {
-            String[] labels = instruction.split("\\s+");    //Each
+            String[] labels = instruction.split("\\s+");    //Each instruction line is split in it's components (labels)
+            //The first component is the type of the element, so we will make a switch statement according to it
             switch (labels[0]) {
-                case "GAIN":
-                    if (!hashMap.containsKey(labels[1].hashCode())) {
-                        hashMap.put(labels[1].hashCode(), new Signal());
+                case "GAIN":    //When there is a gain type
+                    //Output is the second component
+                    if (!hashMap.containsKey(labels[1].hashCode())) {   //If its hashcode isn't added to the hashmap, we add it
+                        hashMap.put(labels[1].hashCode(), new Signal());    //Add the hashcode of the second component to the hashmap and associate it a new Signal
                         k++;
                     }
+                    //Input is the third component
                     if (!hashMap.containsKey(labels[2].hashCode())) {
                         hashMap.put(labels[2].hashCode(), new Signal());
                         k++;
                     }
+                    //We add to the elements arraylist a new Gain element which has as output and input the corresponding Signals
+                    //of the hashcodes from the hashmap.
+                    // The number by which the Gain is done is obtained by downcasting from Element to Gain
                     elements.add(new Gain((Signal) hashMap.get(labels[1].hashCode()), (Signal) hashMap.get(labels[2].hashCode()), Double.parseDouble(labels[3])));
                     break;
                 case "DELAY":
@@ -71,9 +73,10 @@ public class LinearSystem {
                         hashMap.put(labels[2].hashCode(), new Signal());
                         k++;
                     }
+                    //We will create an array of samples starting from the third component
                     double[] samples = new double[labels.length];
-                    for (int i = 3; i<labels.length; i++) {
-                        samples[i-3] = Double.parseDouble(labels[i]);
+                    for (int i = 3; i < labels.length; i++) {
+                        samples[i - 3] = Double.parseDouble(labels[i]);
                     }
                     elements.add(new Filter((Signal) hashMap.get(labels[1].hashCode()), (Signal) hashMap.get(labels[2].hashCode()), samples));
                     break;
@@ -82,14 +85,16 @@ public class LinearSystem {
                         hashMap.put(labels[1].hashCode(), new Signal());
                         k++;
                     }
-                    Signal[] toAdd = new Signal[labels.length-2];
-                    for (int i = 2; i<labels.length; i++) {
+                    //We will create a vector of Signals of length components-2 because the first 2 components are element type and output
+                    Signal[] toAdd = new Signal[labels.length - 2];
+                    for (int i = 2; i < labels.length; i++) {
                         if (!hashMap.containsKey(labels[i].hashCode())) {
                             hashMap.put(labels[i].hashCode(), new Signal());
                             k++;
                         }
-                        toAdd[i-2] = (Signal) hashMap.get(labels[i].hashCode());
+                        toAdd[i - 2] = (Signal) hashMap.get(labels[i].hashCode());
                     }
+                    //Add a new element having the second component as output and the created vector as input
                     elements.add(new Adder((Signal) hashMap.get(labels[1].hashCode()), toAdd));
                     break;
                 case "IN":
@@ -97,6 +102,7 @@ public class LinearSystem {
                         hashMap.put(labels[1].hashCode(), input);
                         k++;
                     }
+                    //The input adress of our system is the adress of the second component
                     this.input = (Signal) hashMap.get(labels[1].hashCode());
                     break;
                 case "OUT":
@@ -104,13 +110,12 @@ public class LinearSystem {
                         hashMap.put(labels[1].hashCode(), new Signal());
                         k++;
                     }
+                    //The output adress is the adress of the second component
                     this.output = (Signal) hashMap.get(labels[1].hashCode());
                     break;
 
             }
         }
-
-
 
 
     }
@@ -120,11 +125,11 @@ public class LinearSystem {
     }
 
     public void compute() { //Compute method
-        int i = 0;
-        System.out.println(this.input);
+
+        //System.out.println(this.input);
         //This method calls the compute of all the elements whose inputs are not empty signals, untill the output is not an empty signal anymore
         while (this.output.samples == null) {   //While the output is an empty signal
-            //System.out.println(++i);
+
             for (Element element : elements) {  //Foreach loop
                 if (!element.getClass().getSimpleName().equals("Adder")) {  //For elements that are not adders and have just one input signal
                     if (element.singleInput.samples != null) {  //Check if the input is empty signal
@@ -140,9 +145,7 @@ public class LinearSystem {
     }
 
     public void setInput(Signal a) {
-
         input.copy(a);  //We want to keep the adress of the input instantiated as the field of linearsystem class, for further use
-
     }
 
     public void setOutput(Signal output) {
@@ -160,14 +163,14 @@ public class LinearSystem {
     public void save() throws IOException {
 
         BufferedWriter bf = null;
-        HashMap hashMap = new HashMap();
-        hashMap.put(input.hashCode(), "in");
-        hashMap.put(output.hashCode(), "out");
-        int i = 0;
+        HashMap hashMap = new HashMap();    //We will use a hashmap to store the hashcodes of the used variables and give these hashcodes certain characters
+        hashMap.put(input.hashCode(), "in");    //Associate input's hashcode with the string "in"
+        hashMap.put(output.hashCode(), "out");  //Associate output's hashcode with the string "out"
+        int i = 0;  //Counter which increments for each added character and is used to generate characters in order
 
         for (Element element : elements) {
             if (!hashMap.containsKey(element.output.hashCode())) {
-
+                //Add to 'a' char the value of i to get another char, depending on number of characters and associate to the obtained char the hashcode of the output
                 hashMap.put(element.output.hashCode(), (char) ('a' + i));
                 i++;
             }
@@ -209,7 +212,7 @@ public class LinearSystem {
                     break;
                 case "Filter":
                     line += "FILTER\t" + hashMap.get(element.output.hashCode()) + "\t" + hashMap.get(element.singleInput.hashCode()) + "\t";
-                    for (double sample:((Filter)element).samples) {
+                    for (double sample : ((Filter) element).samples) {
                         line += sample + "\t";
                     }
                     break;
@@ -251,7 +254,7 @@ public class LinearSystem {
         }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         LinearSystem ls = new LinearSystem("instructions.txt");
 
 
@@ -277,7 +280,6 @@ public class LinearSystem {
         ls.add(new Gain(e, a, 2));
         ls.add(new Adder(p, k, e));
         ls.add(new Gain(n, f, 2));*/
-
 
 
         ls.setInput(a);
